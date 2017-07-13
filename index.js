a091d54f4df2284a50ba9e2838b471ae8c6acc80
Aditya Anand <aditya.anandmc@gmail.com>
2017-07-13 16:20:06 +0530

    fs: added capability to write output to file

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const crawler = require('./controller/crawler.js');
       purifier = require('./controller/purifier.js'),
       utils = require('./controller/utils.js'),
       test = require('./test.js'),
+      fs = require('fs'),
       _ = require("underscore");
 
-const shipments = utils.setDefaultCarrierUrl(test.shipments);
+const inputDataSet = test.shipments;
+const shipments = utils.setDefaultCarrierUrl(inputDataSet);
 let carriersShipment = utils.groupByCarrier(shipments);
 
 carriersShipment = utils.splitCarrierShipments(carriersShipment);
-
+let dataSet = [];
 _.each(carriersShipment, (carrier) => {
   let url = carrier.url;
   const shipmentIds = utils.getKeyProperties(carrier.shipments, "trackingId");
   crawler.headlessCrawl(shipmentIds, url).then((content) => {
-    console.log(purifier.extractDataSet(content, carrier.shipments));
+    purifier.extractDataSet(content, carrier.shipments).then((_dataSet) => {
+      dataSet = dataSet.concat(_dataSet);
+      if(dataSet.length == inputDataSet.length) {
+        fs.writeFile("./_trackingOutput.json", JSON.stringify(dataSet, null, 2), function(err) {
+          if(err) return console.log(err);
+          console.log("shacking complete");
+        });
+      }
+    });
   });
 });
